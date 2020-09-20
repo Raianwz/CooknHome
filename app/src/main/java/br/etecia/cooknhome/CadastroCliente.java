7a03e109ec465623490bbe5635a7d6eda98d467d
raianwz <raian03@live.com>
2020-09-20 13:48:47 -0300

    Criação das Telas de Cadastro e Ajustes

---
+++ app/src/main/java/br/etecia/cooknhome/CadastroCliente.java
@@ -0,0 +1,14 @@
+package br.etecia.cooknhome;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CadastroCliente extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cadastro_cliente_layout);
+    }
+}
---
+++ app/src/main/java/br/etecia/cooknhome/CadastroCozinheiro.java
@@ -0,0 +1,14 @@
+package br.etecia.cooknhome;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CadastroCozinheiro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cadastro_cozinheiro_layout);
+    }
+}
