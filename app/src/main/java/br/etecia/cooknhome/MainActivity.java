5824af0ef5a65fff96b74cf9f6f8c9b7af6b8e67
raianwz <raian03@live.com>
2020-09-20 10:52:54 -0300

    Criaçaão das Telas Splash, Login e Escolha

---
+++ app/src/main/java/br/etecia/cooknhome/MainActivity.java
@@ -0,0 +1,31 @@
+package br.etecia.cooknhome;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    TextView txtCriarConta;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txtCriarConta = findViewById(R.id.txtCriarConta);
+        txtCriarConta.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.txtCriarConta:
+                startActivity(new Intent(getApplicationContext(), TelaEscolha.class));
+        }
+    }
+}
